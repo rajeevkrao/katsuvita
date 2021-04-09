16bb4f80fe26708897df91dd831d3cf1363facbd
Rajeev <rajeevraok@gmail.com>
2021-04-09 16:03:19 +0530

    scroll fixes

--- public/js/page.js
+++ public/js/page.js
@@ -1,7 +1,7 @@
 //document.querySelector("#nav-space").style.height = document.querySelector("nav").offsetHeight+"px";
 
 $(function() {
-   $('.grid').mousewheel(function(event, delta) {
+   $('.scroll').mousewheel(function(event, delta) {
   		this.scrollLeft -= (delta * 30);
 		event.preventDefault();    	
    });
